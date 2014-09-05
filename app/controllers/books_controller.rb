f197e333719b1d986bb883a394e5d157cce932e6
remon87 <remon.eshak2evanschambers.com>
2014-09-04 20:12:31 -0400

    Showing a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,12 @@
 class BooksController < ApplicationController
 
-def index
-@available_at = Time.now
-@books = Book.all
+	def index
+		@available_at = Time.now
+		@books = Book.all
+	end
 
-end
+	def show
+		@book = Book.find(params[:id])
+	end
 
 end
